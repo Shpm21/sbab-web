7a3debb3ce7163160fc864c1a1f47f1076be25c5
Shpm21 <galleguilloscristian2@gmail.com>
2023-01-18 12:38:14 -0300

    create ApplicationSection component

---
+++ components/ApplicationSection.tsx
@@ -0,0 +1,14 @@
+import GenericContainer from './GenericContainer'
+import React from 'react'
+
+const ApplicationSection: React.FC = () => {
+  return (
+    <>
+      <GenericContainer>
+        <div>Testing Application Section</div>
+      </GenericContainer>
+    </>
+  )
+}
+
+export default ApplicationSection
