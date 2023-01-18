cca4cae25179de7ba28362856051337567af447d
Shpm21 <galleguilloscristian2@gmail.com>
2023-01-18 12:37:25 -0300

    create app page

---
+++ app/application/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ApplicationSection from '../../components/ApplicationSection'
+
+const ApplicationPage: React.FC = () => {
+  return (
+    <>
+      <ApplicationSection />
+    </>
+  )
+}
+
+export default ApplicationPage
