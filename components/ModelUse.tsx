1e1592b03120134f6f21a19cc99d9a25b5cde504
Shpm21 <galleguilloscristian2@gmail.com>
2023-01-18 11:20:18 -0300

    add ModelUsage Image

--- components/ModelUse.tsx
+++ components/ModelUse.tsx
@@ -3,14 +3,29 @@ import { modelUseText1, modelUseText2 } from '../utils/textData'
 import Subtitle from './Subtitle'
 import Paragraph from './Paragraph'
 import GenericContainer from './GenericContainer'
+import { getModelUsageImage } from '../utils/data'
+import Image from 'next/image'
 
 const ModelUse: React.FC = () => {
+  const modelUsageImage = getModelUsageImage()
   return (
     <>
-      <GenericContainer
-      >
+      <GenericContainer>
         <Subtitle subTitle={'Model Usage'} />
         <Paragraph paragraph={modelUseText1} justify={true} gridCols2={false} />
+        <div
+          className="
+          mt-6 grid grid-cols-1
+          place-items-center
+        "
+        >
+          <Image
+            src={modelUsageImage}
+            alt={'model-image'}
+            width={800}
+            height={800}
+          />
+        </div>
         <Paragraph paragraph={modelUseText2} justify={true} gridCols2={false} />
       </GenericContainer>
     </>
