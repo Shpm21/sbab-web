16fd1a45fe0d1ea923698386e29f619d2e7ae23a
Shpm21 <galleguilloscristian2@gmail.com>
2023-04-02 21:16:31 -0400

    update video

--- components/VideoE.tsx
+++ components/VideoE.tsx
@@ -3,7 +3,7 @@ import Spline from '@splinetool/react-spline';
 const VideoE = () => {
 
   return (
-    <Spline scene="https://prod.spline.design/GIa96wGpko4WRb95/scene.splinecode"
+    <Spline scene=""
     />
   );
 }
