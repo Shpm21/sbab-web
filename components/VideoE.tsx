b5c85c260d35a4dbefe427cc263bb8d4639e773f
Shpm21 <galleguilloscristian2@gmail.com>
2023-04-02 21:17:22 -0400

    update

--- components/VideoE.tsx
+++ components/VideoE.tsx
@@ -1,6 +1,6 @@
 import Spline from '@splinetool/react-spline';
 
-const VideoE = () => {
+const VideoE: React.FC = () => {
 
   return (
     <Spline scene="https://prod.spline.design/GIa96wGpko4WRb95/scene.splinecode"
